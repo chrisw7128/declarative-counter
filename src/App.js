e3adbe8a1500cfca8b2841fac7105aa34743b060
Chris Wehner <chrisw7128@gmail.com>
2023-05-10 21:11:33 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let [counter, setCounter] = useState(0);
+  window.setCounter = setCounter;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{counter}</h1>
       </header>
     </div>
   );
